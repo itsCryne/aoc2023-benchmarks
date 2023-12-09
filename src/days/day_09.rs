395739f0cc32b526e9977e523692db8ef9a1ebde
Patrick <secure@cryne.me>
2023-12-09 08:44:58 +0100

    Modify Day 09

--- src/days/day_09.rs
+++ src/days/day_09.rs
@@ -1,55 +1,49 @@
 pub fn part_a(input: &str) -> Option<i64> {
-    let oasis_lines: Vec<Vec<i64>> = input
-        .lines()
-        .map(|l| l.split_whitespace().map(|n| n.parse().unwrap()).collect())
-        .collect();
-
-    let mut sum = 0;
-    for report in oasis_lines {
-        let mut histories = vec![report];
-        while histories.last().unwrap().iter().sum::<i64>() != 0 {
-            let mut last_history_diffs = vec![];
-            for vals in histories.last().unwrap().as_slice().windows(2) {
-                last_history_diffs.push(vals[1] - vals[0]);
-            }
-            histories.push(last_history_diffs);
-        }
-
-        histories.reverse();
-        let mut extrapolate_vals = vec![0];
-        for history in &histories[1..] {
-            extrapolate_vals.push(history.last().unwrap() + extrapolate_vals.last().unwrap());
-        }
-        sum += extrapolate_vals.last().unwrap();
-    }
-    Some(sum)
+    Some(
+        input
+            .lines()
+            .map(|l| l.split_whitespace().map(|n| n.parse().unwrap()).collect())
+            .map(|report: Vec<i64>| {
+                std::iter::successors(Some(report), |last| {
+                    if last.iter().sum::<i64>() == 0 {
+                        None
+                    } else {
+                        Some(last.windows(2).map(|w| w[1] - w[0]).collect())
+                    }
+                })
+                .map(|h| *h.last().unwrap())
+                .collect::<Vec<_>>()
+                .into_iter()
+                .rev()
+                .reduce(|acc, e| acc + e)
+                .unwrap()
+            })
+            .sum(),
+    )
 }
 
 pub fn part_b(input: &str) -> Option<i64> {
-    let oasis_lines: Vec<Vec<i64>> = input
-        .lines()
-        .map(|l| l.split_whitespace().map(|n| n.parse().unwrap()).collect())
-        .collect();
-
-    let mut sum = 0;
-    for report in oasis_lines {
-        let mut histories = vec![report];
-        while histories.last().unwrap().iter().sum::<i64>() != 0 {
-            let mut last_history_diffs = vec![];
-            for vals in histories.last().unwrap().as_slice().windows(2) {
-                last_history_diffs.push(vals[1] - vals[0]);
-            }
-            histories.push(last_history_diffs);
-        }
-
-        histories.reverse();
-        let mut extrapolate_vals = vec![0];
-        for history in &histories[1..] {
-            extrapolate_vals.push(history.first().unwrap() - extrapolate_vals.last().unwrap());
-        }
-        sum += extrapolate_vals.last().unwrap();
-    }
-    Some(sum)
+    Some(
+        input
+            .lines()
+            .map(|l| l.split_whitespace().map(|n| n.parse().unwrap()).collect())
+            .map(|report: Vec<i64>| {
+                std::iter::successors(Some(report), |last| {
+                    if last.iter().sum::<i64>() == 0 {
+                        None
+                    } else {
+                        Some(last.windows(2).map(|w| w[1] - w[0]).collect())
+                    }
+                })
+                .map(|h| *h.first().unwrap())
+                .collect::<Vec<_>>()
+                .into_iter()
+                .rev()
+                .reduce(|acc, e| e - acc)
+                .unwrap()
+            })
+            .sum(),
+    )
 }
 
 #[cfg(test)]
